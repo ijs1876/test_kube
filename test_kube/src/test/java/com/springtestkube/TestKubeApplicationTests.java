7c84479467491025ae1669bc49c106d0cdeef63c
i1876 <i1876@ijs>
2022-05-31 09:26:46 +0900

    init

---
+++ test_kube/src/test/java/com/springtestkube/TestKubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtestkube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestKubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
